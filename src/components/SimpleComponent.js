2a7f5a716cdd0130d9c5701bc8a1593456856098
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-21 16:26:34 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,15 @@
-// Code SimpleComponent Here
+
+const defaultLimit = 100; 
+ 
+class SimpleComponent extends Component {
+  super()
+  render() {
+    return (
+      <input
+        className="field field-light"
+        onChange={this.props.onChange}
+        maxLength={this.props.limit || defaultLimit}
+      />
+    );
+  }
+}
