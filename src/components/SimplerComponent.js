9638947fe542f122dc28a9f8a987c3228f19cda2
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-21 16:35:52 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,8 @@
 import React from 'react';
+
+const SimplerComponent = (props) =>
+  <div
+    className="field field-light"
+    onChange={props.onChange}
+    limit={props.limit || defaultLimit}
+  />;
