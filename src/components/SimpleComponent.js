b51b2852efdac936e485d86e4db59d9835dd9f6e
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-21 16:40:44 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -15,8 +15,7 @@ class SimpleComponent extends Component {
   }
   
   render() {
-    return (
-      <div onClick={this.handleClick}/>
+    return <div onClick={this.handleClick}>
         {this.state.mood}
       </div> 
   }
